cb779522ec5fb8557d1c77836342e33143568c6a
yene <yene@cocoadojo.com>
2015-06-29 19:12:49 +0200

    added matrix for the pieces

--- Game.swift
+++ Game.swift
@@ -9,8 +9,17 @@
 import Foundation
 import SpriteKit
 
+enum Piece {
+  case Attacker
+  case Defender
+  case King
+  case Corner
+  case Throne
+  case Empty
+}
+
 class Game {
-  var board = Matrix(rows: 11, columns: 11)
+  var board = PieceMatrix(rows: 11, columns: 11)
   
   func checkWinCondition() {
    // king in corner = win for defender
@@ -24,4 +33,27 @@ class Game {
     // special: all defenders are encircled, leads to defeat i guess anyway
   }
   
+}
+
+struct PieceMatrix {
+  let rows: Int, columns: Int
+  var grid: [Piece]
+  init(rows: Int, columns: Int) {
+    self.rows = rows
+    self.columns = columns
+    grid = Array(count: rows * columns, repeatedValue: Piece.Empty)
+  }
+  func indexIsValidForRow(row: Int, column: Int) -> Bool {
+    return row >= 0 && row < rows && column >= 0 && column < columns
+  }
+  subscript(row: Int, column: Int) -> Piece {
+    get {
+      assert(indexIsValidForRow(row, column: column), "Index out of range")
+      return grid[(row * columns) + column]
+    }
+    set {
+      assert(indexIsValidForRow(row, column: column), "Index out of range")
+      grid[(row * columns) + column] = newValue
+    }
+  }
 }
